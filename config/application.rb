0e0b805443d72a748d00905e09fa9e50f20ad717
AAlrifae <a.a.alrifae@gmail.com>
2019-08-19 16:38:29 +0300

    Second Deploy

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,17 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
 module VolunteerApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
-    config.serve_static_files = false
     config.assets.compile = false
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
