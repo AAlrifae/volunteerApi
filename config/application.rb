744f703e32b186af5ebeb58cd9fcd2fc48876940
AAlrifae <a.a.alrifae@gmail.com>
2019-08-19 16:50:08 +0300

    Second Deploy

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.assets.compile = false
 
 module VolunteerApi
   class Application < Rails::Application
@@ -12,7 +13,6 @@ module VolunteerApi
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
-    config.assets.compile = false
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
