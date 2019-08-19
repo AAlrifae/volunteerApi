468fdadecee177a4d99b56ca79a6d2594bb10008
AAlrifae <a.a.alrifae@gmail.com>
2019-08-19 16:25:16 +0300

    Second Deploy

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -22,7 +22,7 @@ Devise.setup do |config|
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
-  config.secret_key = '-- secret key --'
+
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
 
