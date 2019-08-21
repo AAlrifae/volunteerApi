4ea7e9f084224b4c2b09651921cef84f1e4fbd96
AAlrifae <a.a.alrifae@gmail.com>
2019-08-21 16:25:36 +0300

    show all the user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @events = User.all
+    user = User.all
+    render json: user
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
+
 Rails.application.routes.draw do
 
   # API routes path
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       # devise_for :users
-      resources :users, :only => [:show, :create]
+      resources :users, :only => [:show, :create, :index]
     end
   end  
 end
