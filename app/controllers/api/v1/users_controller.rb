c6008648851812eb108ff00a472d5a410d0283dc
AAlrifae <a.a.alrifae@gmail.com>
2019-08-21 16:37:45 +0300

    show all the user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,34 +11,19 @@ class Api::V1::UsersController < ApplicationController
     respond_with User.find(params[:id])
   end
 
-  # def create
-  #   user = User.new(user_params)
-  #   # if the user is saved successfully than respond with json data and status code 201
-  #   if user.save
-  #     render json: user, status: 201
-  #   else
-  #     render json: { errors: user.errors }, status: 422
-  #   end
-  # end
-  #----------------------------------------
   def create
-    user = User.new(event_params)
-
-    respond_to do |format|
-      if user.save
-        format.html { redirect_to user, notice: "Event was successfully created." }
-        format.json { render :show, status: :created, location: user }
-      else
-        format.html { render :new }
-        format.json { render json: user.errors, status: :unprocessable_entity }
-      end
+    user = User.new(user_params)
+    # if the user is saved successfully than respond with json data and status code 201
+    if user.save
+      render json: user, status: 201
+    else
+      render json: { errors: user.errors }, status: 422
     end
   end
 
-  #==============================================
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :age, :userName, :phoneNumber, :location, :gender)
+    params.require(:user).permit(:email, :password, :password_confirmation, :age, :userName, :phoneNumber, :location, :gender, :isOrganization)
   end
 end
