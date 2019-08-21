bc086f3ca14ff580934fc0676cbfe97b160837a3
AAlrifae <a.a.alrifae@gmail.com>
2019-08-21 17:56:36 +0300

    add cors gem file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,1 +1,10 @@
-# c
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "http://localhost:3000"
+    resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head], credentials: true
+  end
+  allow do
+    origins "https://volunteery-api.herokuapp.com"
+    resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head], credentials: true
+  end
+end
