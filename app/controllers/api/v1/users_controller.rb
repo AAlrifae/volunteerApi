86601a327d9926c9e35069d669dd5dc1eda34da3
AAlrifae <a.a.alrifae@gmail.com>
2019-08-21 16:05:51 +0300

    edit api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,24 +1,24 @@
 class Api::V1::UsersController < ApplicationController
-    respond_to :json
-    skip_before_action  :verify_authenticity_token
+  respond_to :json
+  skip_before_action :verify_authenticity_token
 
-  
-    def show
-      respond_with User.find(params[:id])
-    end
-  
-    def create
-     user=User.new(user_params) 
-          # if the user is saved successfully than respond with json data and status code 201
-          if user.save 
+  def show
+    respond_with User.find(params[:id])
+  end
+
+  def create
+    user = User.new(user_params)
+    # if the user is saved successfully than respond with json data and status code 201
+    if user.save
       render json: user, status: 201
-     else
-      render json: { errors: user.errors}, status: 42
-     end
+    else
+      render json: { errors: user.errors }, status: 422
     end
-  
-    private
-    def user_params
-     params.require(:user).permit(:email, :password, :password_confirmation, :age, :userName, :phoneNumber, :location, :gender, :isOrganization ) 
-          end
-  end 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :age, :userName, :phoneNumber, :location, :gender, :isOrganization)
+  end
+end
